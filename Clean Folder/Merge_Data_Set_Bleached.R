908b09ecf1ccc7016c6ee05e9f90d199770f54ef
niwech97 <niwech97@gmail.com>
2020-02-17 17:32:01 -0800

    DONE THE LOOP

--- Clean Folder/Merge_Data_Set_Bleached.R
+++
@@ -1,5 +0,0 @@
-test.total <- merge(GDP15, MCR15, by=c("LOCATION", "TIME"))
-head(test.total)
-<- na.omit(test.total)
-
-write.csv(test.total, file=paste(path.clean,"Final Dataset.csv",sep="/"))
--- Relevant Analysis/Final Analysis.R
+++ Relevant Analysis/Final Analysis.R
@@ -32,6 +32,47 @@ dev.off()
 #we decided to perform a linear regression model on this dataset to
 #determine if there is a relationship between vaccination rates and time
 
-plot(lm(m$TIME~m$Value, xlab="Year",ylab="Vaccinated Children(%)",
-     main="Linear Regression of Global Child Vaccination Rate 1980-2018",
-     las=1, pch=16,col="black"))
+
+#creating a results object to store our results
+model.1.results <-rep(NA,length(m$LOCATION))
+
+#this is the overall model we are looking at
+model.1 <- lm(m$TIME~m$Value)
+model.1.sum <- summary(model.1)
+
+#save results and send to result folder if possible
+
+#attempt to save model.1.sum as a CSV
+write.csv(model.1.sum, paste(path.clean,"Total Linear Regression.csv",sep="/"))
+
+plot(lm(m$TIME~m$Value))
+#create a loop for this to be done on every country
+# first make one for the entire dataset just like we did above
+#
+unique.country<-unique(m$LOCATION)
+
+
+for(i in 1:length(m$LOCATION)){
+for(ctr in 1:length(unique.country)){
+ if(m$LOCATION[i] == unique.country[ctr]){
+  graph.temp<-subset(m,subset = LOCATION ==unique.country[ctr])
+  pdf(file=paste(path.fingraph,"/",m$LOCATION[i],".pdf",sep=""))
+  plot(graph.temp$TIME,graph.temp$Value,
+       main=unique.country[ctr],xlab="Year",ylab= "Vaccination Rate (%)")
+dev.off()
+}}}
+?dev.list
+
+
+
+# save for later to edit
+   #for(i in 1:length(m$LOCATION)){
+   ctr<-1
+   i<-1
+    if(dataset$TIME[i] == unique.years[ctr]){
+     graph.temp<-subset(dataset,subset = TIME==unique.years[ctr])
+     pdf(file=paste(path.fingraph,"/",dataset$TIME[i],".pdf",sep=""))
+     plot(graph.temp$Value.y,graph.temp$Value.x,main=unique.years[ctr],
+          xlab="Year",ylab= "Vaccination Rate")}
+   dev.off()
+      }}
