eda669b74fbff744eaacba883d5d3addec4902c3
niwech97 <niwech97@gmail.com>
2020-02-18 10:06:39 -0800

    Update Main.R

--- Final R scripts/Main.R
+++ Final R scripts/Main.R
@@ -18,6 +18,40 @@ library(dplyr)
 library(ggplot2)
 
 
-#Main script----------
+#Main script-------------
+#Reading in raw data-----
+m<-read.csv(file=paste(path.rd,"/","Measles_ALL.csv",sep=""))
 
 
+#Cleaning data-----------
+m<-select(m, -c("SUBJECT", "INDICATOR", "MEASURE", "Flag.Codes",
+                "FREQUENCY"))
+
+#Creating the loop-------
+#we created an object with only the names of the relevant 
+#countries to be used in our loop
+unique.country<-unique(m$LOCATION)  
+
+
+#we used a nested loop structure to accomplish our loop.
+#first we started a counter to do something in every row of our dataset
+for(i in 1:length(m$LOCATION)){               
+  #then we used a second counter to utilize our unique country list
+  for(ctr in 1:length(unique.country)){
+    #within that counter we created an if statement to extract only 
+    #the data from one country at a time 
+    if(m$LOCATION[i] == unique.country[ctr]){
+      #we stored the subset of one country at a time in a temporary object
+      graph.temp<-subset(m,subset = LOCATION ==unique.country[ctr])
+      #then we opened a PDF file to save it as the name of the country in our Relevant Figures folder
+      pdf(file=paste(path.fingraph,"/",m$LOCATION[i],".pdf",sep=""))
+      #Finally we plotted the graph of each countries vaccination rate over time
+      plot(graph.temp$TIME,graph.temp$Value,
+           main=unique.country[ctr],xlab="Year",ylab= "Vaccination Rate (%)",
+           pch=16, las=1, col="blue")
+      #and turned off the device
+      dev.off()
+    }}}
+
+
+#Performing relevant analysis--------------------------------------------
